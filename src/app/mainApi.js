a6d919dd6bd85ca0ec8662da1897b58f2d3013b5
Inima <inima@gmail.com>
2025-05-30 13:08:42 +0545

    changes

--- src/app/mainApi.js
+++ src/app/mainApi.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-export const baseUrl = 'http://192.168.1.8:5000';
-//export const baseUrl = 'https://shops-f44f.onrender.com';
+//export const baseUrl = 'http://192.168.1.122:5000';
+export const baseUrl = 'https://shops-f44f.onrender.com';
 
 
 export const mainApi = createApi({
